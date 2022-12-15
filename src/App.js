06e1848f1bc00cd02180889ca65381e47ba57242
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-12-15 19:27:07 +0700

    openai: update UI

--- src/App.js
+++ src/App.js
@@ -1,138 +1,114 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 const App = () => {
   const [prompt, setPrompt] = useState("");
   const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
   const configuration = new Configuration({
     apiKey: process.env.REACT_APP_API_KEY,
   });
 
-  // useEffect(() => {
-  //   generateImage();
-  // }, []);
-
   const openai = new OpenAIApi(configuration);
 
-  // const generateImage = async () => {
-  //   const res = await openai.createImage({
-  //     prompt: prompt,
-  //     n: 1,
-  //     size: "1024x1024",
-  //   });
-  //   // setResult(res.data.data[0].url);
-  //   // console.log(res.data);
-  // };
-
   const generateCompletion = async () => {
+    setLoading(true);
     const res = await openai.createCompletion({
       prompt: prompt,
       model: "text-davinci-003",
-      temperature: 0.6,
+      temperature: 0.9,
       max_tokens: 2048,
     });
+    setLoading(false);
     setResult(res.data.choices[0].text);
-    console.log(res.data);
+    // console.log(res.data);
   };
 
   return (
     <>
-      {/* <textarea onChange={(e) => setPrompt(e.target.value)} /> */}
-      {/* <button onClick={generateImage}>Generate Image</button> */}
-      {/* {result.length > 0 ? <img src={result} /> : <></>} */}
-
-      {/* <button onClick={generateCompletion}>Generate Completion</button> */}
-      {/* {result.length > 0 ? <i>{result}</i> : <></>} */}
-
-      <div className="m-5 p-5 relative">
-        <label className="block mb-2 text-sm font-medium text-white">
+      <div className="mt-5 text-center">
+        <h1 className="font-medium underline underline-offset-8">
+          OpenAI - ChatGPT
+        </h1>
+        <p className="text-xs mt-3">
+          Made with <FontAwesomeIcon icon={faHeart} className="text-red-600" />{" "}
+          by Arman
+        </p>
+      </div>
+      <div className="mr-5 ml-5 p-5 relative">
+        <label className="block mb-2 text-sm font-normal">
           Give AI Question
         </label>
-        <input
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <div className="absolute right-3 mt-5">
-          <button
-            type="button"
-            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            onClick={generateCompletion}
-          >
-            Send
-          </button>
+        <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+          <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+            <textarea
+              id="comment"
+              rows="7"
+              className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+              placeholder="Write a comment..."
+              required
+              onChange={(e) => setPrompt(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="flex items-end justify-end px-3 py-2 border-t dark:border-gray-600">
+            <button
+              type="submit"
+              className="flex items-end py-2.5 px-4 text-xs font-medium text-center text-white bg-green-700 rounded-lg focus:ring-4 focus:ring-green-200 dark:focus:ring-green-900 hover:bg-green-800"
+              onClick={generateCompletion}
+            >
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+          </div>
         </div>
       </div>
 
-      {result.length > 0 ? (
-        <div className="m-5 p-5">
-          <label className="block mb-2 text-sm font-medium text-white">
-            Result
-          </label>
-          <blockquote className="mt-5 p-4 my-4 bg-gray-400 border-l-4 border-gray-300 dark:border-gray-500 dark:bg-gray-800">
-            <p className="text-base font-medium leading-relaxed text-gray-900 dark:text-white">
-              {result}
-            </p>
-          </blockquote>
-        </div>
-      ) : (
-        <></>
-      )}
-
-      {/* <form>
-        <div className="mb-6">
-          <label
-            for="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            for="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
-        </div>
-        <div className="flex items-start mb-6">
-          <div className="flex items-center h-5">
-            <input
-              id="remember"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-              required
-            />
+      {loading ? (
+        <>
+          <div className="flex flex-col items-center">
+            <button
+              disabled
+              type="button"
+              className="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 items-center"
+            >
+              <svg
+                role="status"
+                className="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="#1C64F2"
+                />
+              </svg>
+              Loading...
+            </button>
           </div>
-          <label
-            for="remember"
-            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Remember me
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
-      </form> */}
+        </>
+      ) : (
+        <>
+          {result.length > 0 ? (
+            <div className="mr-5 ml-5 px-4">
+              <label className="block mb-2 text-sm font-medium">Result</label>
+              <blockquote className="p-4 my-4 bg-gray-200 border-l-4 border-green-300 dark:border-green-300 dark:bg-gray-600">
+                <p className="text-base font-medium leading-relaxed text-gray-900 dark:text-white">
+                  {result}
+                </p>
+              </blockquote>
+            </div>
+          ) : (
+            <></>
+          )}
+        </>
+      )}
     </>
   );
 };
