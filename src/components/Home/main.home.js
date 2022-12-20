22d18230921ed8f07ffdbe295ff56491a40d381a
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-12-21 02:17:44 +0700

    change button color

--- src/components/Home/main.home.js
+++ src/components/Home/main.home.js
@@ -12,7 +12,7 @@ const Home = (props) => {
       <NavLink to="/chatgpt">
         <button
           type="button"
-          class="mt-5 w-36 md:w-52 md:h-15 md:text-2xl font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 transition ease-in-out delay-150 bg-indigo-600 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-700 duration-300 text-white"
+          className="mt-5 w-36 md:w-52 md:h-15 md:text-2xl font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 transition ease-in-out delay-150 bg-blue-600 hover:-translate-y-1 hover:scale-110 hover:bg-blue-700 duration-300 text-white"
         >
           Let's Go
         </button>
