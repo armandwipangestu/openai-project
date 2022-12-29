aa7c9f2c5fcb7be069427da552e05b62d12e1501
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-12-29 18:59:45 +0700

    openai: add Neu-Brutalism UI for ChatGPT 2

--- src/components/ChatGPT/completion.chatgpt.js
+++ src/components/ChatGPT/completion.chatgpt.js
@@ -9,6 +9,18 @@ const Completion = () => {
   const [prompt, setPrompt] = useState("");
   const [result, setResult] = useState("");
   const [loading, setLoading] = useState(false);
+  const [chatLog, setChatLog] = useState([
+    // {
+    //   user: "gpt",
+    //   message: "How can I help you Today?",
+    //   avatar: "openai-dark.png",
+    // },
+    // {
+    //   user: "me",
+    //   message: "I want to use ChatGPT",
+    //   avatar: "me.png",
+    // },
+  ]);
   const configuration = new Configuration({
     apiKey: process.env.REACT_APP_API_KEY,
   });
@@ -16,6 +28,9 @@ const Completion = () => {
   const openai = new OpenAIApi(configuration);
 
   const generateCompletion = async () => {
+    let chatLogNew = [...chatLog, { user: "me", message: prompt }];
+    setChatLog(chatLogNew);
+    setPrompt("");
     setLoading(true);
     const res = await openai.createCompletion({
       prompt: prompt,
@@ -24,119 +39,101 @@ const Completion = () => {
       max_tokens: 2048,
     });
     setLoading(false);
-    setResult(res.data.choices[0].text);
+    setChatLog([
+      ...chatLogNew,
+      { user: "gpt", message: `${res.data.choices[0].text}` },
+    ]);
   };
-
-  const dummy = [
-    {
-      user: {
-        image: "me.png",
-        question: "Give me the best programming language at 2022!",
-      },
-      ai: {
-        image: "openai-dark.png",
-        answer:
-          'It is not possible for me to accurately predict which programming language will be considered the "best" in 2022.',
-      },
-    },
-    {
-      user: {
-        image: "me.png",
-        question: "Give me 3 anime name",
-      },
-      ai: {
-        image: "openai-dark.png",
-        answer: "1. Attack on Titan, 2. Death Note, 3. One Piece",
-      },
-    },
-    {
-      user: {
-        image: "me.png",
-        question: "Give me 3 Animal Name",
-      },
-      ai: {
-        image: "openai-dark.png",
-        answer: "1. Cat, 2. Dog, 3. Lion",
-      },
-    },
-    {
-      user: {
-        image: "me.png",
-        question: "Give me the best programming language at 2022!",
-      },
-      ai: {
-        image: "openai-dark.png",
-        answer:
-          'It is not possible for me to accurately predict which programming language will be considered the "best" in 2022.',
-      },
-    },
-    {
-      user: {
-        image: "me.png",
-        question: "Give me 3 anime name",
-      },
-      ai: {
-        image: "openai-dark.png",
-        answer: "1. Attack on Titan, 2. Death Note, 3. One Piece",
-      },
-    },
-    {
-      user: {
-        image: "me.png",
-        question: "Give me 3 Animal Name",
-      },
-      ai: {
-        image: "openai-dark.png",
-        answer: "1. Cat, 2. Dog, 3. Lion",
-      },
-    },
-  ];
 
   return (
     <>
-      <div className="mt-10">
-        {dummy.map((d, i) => (
-          <>
-            <div
-              className="flex p-5 rounded-lg mt-5 mb-5 bg-blue-300 border-2 border-black"
-              style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
-              key={i}
-            >
-              <img src={d.user.image} className="w-6 h-6 mr-3" />
-              <div>
-                <span className="text-black">{d.user.question}</span>
+      <div className="min-h-screen flex flex-col justify-start">
+        <div className="">
+          {chatLog.map((log, i) => (
+            <>
+              {log.user === "me" && (
+                <div
+                  className="flex p-5 rounded-lg mt-5 mb-5 bg-blue-300 border-2 border-black"
+                  style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
+                  key={i}
+                >
+                  <img src="avatar.png" className="w-6 h-6 mr-3" />
+                  <div>
+                    <span className="text-black">{log.message}</span>
+                  </div>
+                </div>
+              )}
+              {log.user === "gpt" && (
+                <div
+                  className="flex p-5 bg-yellow-400 rounded-lg mt-5 mb-5 border-2 border-black"
+                  style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
+                >
+                  <img src="openai-dark.png" className="w-6 h-6 mr-3" />
+                  <div>
+                    <span className="text-black">{log.message}</span>
+                  </div>
+                </div>
+              )}
+            </>
+          ))}
+
+          {loading && (
+            <div className="relative">
+              <div
+                className="flex p-5 bg-yellow-400 rounded-lg mt-5 mb-5 border-2 border-black"
+                style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
+              >
+                <img src="openai-dark.png" className="w-6 h-6 mr-3" />
+                <div className="dot-container">
+                  <span className="dot mr-2"></span>
+                  <span className="dot mr-2"></span>
+                  <span className="dot mr-2"></span>
+                </div>
               </div>
             </div>
-            <div
-              className="flex p-5 bg-yellow-400 rounded-lg mt-5 mb-5 border-2 border-black"
-              style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
+          )}
+        </div>
+
+        <div className="mt-auto flex sticky bottom-5">
+          <div className="flex absolute bottom-0 right-0 pb-2 md:pb-2.5 mr-3">
+            <button
+              type="submit"
+              className="items-end py-1 px-2 text-xs font-medium text-center text-black hover:bg-gray-700 rounded-lg"
+              onClick={generateCompletion}
             >
-              <img src={d.ai.image} className="w-6 h-6 mr-3" />
-              <div>
-                <span className="text-black">{d.ai.answer}</span>
-              </div>
-            </div>
-          </>
-        ))}
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+          </div>
+          <TextareaAutosize
+            className="w-full border-2 border-black rounded-md placeholder-gray-500 resize-none py-2 pl-3 pr-10 md:pl-5 outline-none"
+            placeholder="Enter your message here"
+            style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
+            maxRows={5}
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+        </div>
       </div>
 
-      <div className="flex sticky bottom-14 md:bottom-5 w-full mb-10 md:mb-6">
-        <div className="flex absolute bottom-0 right-0 pb-2 md:pb-2.5 mr-3">
-          <button
-            type="submit"
-            className="items-end py-1 px-2 text-xs font-medium text-center text-black hover:bg-gray-700 rounded-lg"
-            onClick={generateCompletion}
-          >
-            <FontAwesomeIcon icon={faPaperPlane} />
-          </button>
-        </div>
-        <TextareaAutosize
-          className="w-full border-2 border-black rounded-md placeholder-gray-500 resize-none py-2 pl-3 pr-10 md:pl-5 outline-none"
-          placeholder="Enter your message here"
-          style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
-          maxRows={5}
-        />
-      </div>
+      {/* <div className="flex sticky bottom-14 md:bottom-5 w-full mb-10 md:mb-6"> */}
+      {/* <div className="flex absolute bottom-14 md:bottom-5 md:w-9/12 mb-10 md:mb-6"> */}
+      {/* <div className="flex absolute bottom-0 right-0 pb-2 md:pb-2.5 mr-3">
+        <button
+          type="submit"
+          className="items-end py-1 px-2 text-xs font-medium text-center text-black hover:bg-gray-700 rounded-lg"
+          onClick={generateCompletion}
+        >
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </button>
+      </div> */}
+      {/* <TextareaAutosize
+        className="w-full border-2 border-black rounded-md placeholder-gray-500 resize-none py-2 pl-3 pr-10 md:pl-5 outline-none"
+        placeholder="Enter your message here"
+        style={{ boxShadow: "0.4rem 0.4rem 0 #222" }}
+        maxRows={5}
+        onChange={(e) => setPrompt(e.target.value)}
+      /> */}
+      {/* </div> */}
     </>
   );
 };
